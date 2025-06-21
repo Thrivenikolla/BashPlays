34774c88912179491309eb516e7863f8c01c4adb
Sri <thrivenichowdary97@gmail.com>
2025-06-21 13:58:31 +0530

    ifprog added

--- testscripts/ifprog.sh
+++ testscripts/ifprog.sh
@@ -5,3 +5,12 @@ if [ $num -gt 0 ]; then
 else
     echo "Given number $num is negative or zero"
 fi
+
+# Check if a number is even or odd
+if [ $num -eq 0 ]; then
+    echo "Given num is zero"
+elif [ $((num % 2)) -eq 0 ]; then
+    echo "given num is even"
+else
+    echo "given number $num is odd"
+fi
